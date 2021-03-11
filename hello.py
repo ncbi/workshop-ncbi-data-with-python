a707eaff31492d1b6153f1ad417e6b4a4f628c6c
Daniel Rice <daniel.rice@nih.gov>
2021-03-11 13:32:05 -0500

    Add test notebook script.

---
+++ hello.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.10.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Hello world!
+
+print("Hello world!")
+
+# The previous cell should print "Hello world!"

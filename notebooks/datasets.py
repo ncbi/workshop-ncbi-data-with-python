72affdb3147702e0f1ac23b44351aaab329ced81
Daniel Rice <daniel.rice@nih.gov>
2021-04-06 12:57:03 -0400

    Add some datasets content.

--- notebooks/datasets.py
+++ notebooks/datasets.py
@@ -13,6 +13,36 @@
 #     name: python3
 # ---
 
+from collections import Counter
+
 import ncbi.datasets as datasets
+import pandas as pd
 
 help(datasets)
+
+api_instance = datasets.GenomeApi(datasets.ApiClient())
+assembly_accessions = ["GCF_000001405.39"]
+genome_summary = api_instance.assembly_descriptors_by_accessions(
+    assembly_accessions, limit="all"
+)
+print(genome_summary)
+
+print(f"Number of assemblies: {genome_summary.total_count}")
+
+for assembly in map(lambda d: d.assembly, genome_summary.assemblies):
+    print(
+        assembly.assembly_accession,
+        assembly.assembly_level,
+        len(assembly.chromosomes),
+        assembly.submission_date,
+        sep="\t",
+    )
+
+assm_counter = Counter()
+for assembly in map(lambda d: d.assembly, genome_summary.assemblies):
+    if assembly.assembly_accession[:3] == "GCA":
+        assm_counter["GenBank"] += 1
+    elif assembly.assembly_accession[:3] == "GCF":
+        assm_counter["RefSeq"] += 1
+df = pd.DataFrame.from_dict(assm_counter, orient="index", columns=["count"])
+df.plot(kind="pie", y="count", figsize=(6, 6), title="Assemblies by type")
